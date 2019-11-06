39c377afae7d1cb57b58d47be9328c23c9111ab5
Ryan Nett <rnett@calpoly.edu>
2019-11-06 11:21:19 -0800

    initial commit

---
+++ data.py
@@ -0,0 +1,25 @@
+import codecs
+import csv
+import lzma
+import platform
+from pathlib import Path
+
+if platform.system() == "Windows":
+    user = str(Path("~").expanduser()).split("\\")[-1]
+else:
+    pass
+
+if user == 'jimne' or user == 'rnett':
+    data_dir = Path("E:\\autociter")
+elif user == 'JNett':
+    data_dir = Path(
+        "C:\\Users\\JNett\\Common\\Desktop\\Other "
+        "Stuff\\review_comments.csv.xz")
+else:
+    # TODO set this for other users
+    data_dir = None
+
+
+def load():
+    with lzma.open("review_comments.csv.xz") as archf:
+        return csv.DictReader(codecs.getreader("utf-8")(archf))
