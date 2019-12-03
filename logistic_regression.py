0b21e8c07eca5b9c540e74654244207336415832
Nathan Lui <nathanlui2010@gmail.com>
2019-12-03 14:57:30 -0800

    Print in table format

--- logistic_regression.py
+++ logistic_regression.py
@@ -11,8 +11,41 @@ def log_reg(filename, cols):
 def to_percent(num):
   return str(round(num * 100, 2)) + "%"
 
-if __name__ == '__main__':
+def print_table(files, columns):
+  print("\n        ", end=' ')
+  for filepair in files:
+    print(filepair[0], end='   \t')
+  print()
+    
+  for colpair in columns:
+    print(colpair[0], '-', end=' ')
+    for filepair in files:
+      print(to_percent(log_reg(filepair[1], colpair[1])), end='   \t')
+    print()
   
+
+if __name__ == '__main__':
+
+  files = [("Issues ", 'sentiment-issue2.csv'),
+           ("Reviews", 'sentiment-review.csv'),
+           ("Both   ", 'sentiment-both.csv')]
+
+  columns = [("All   ", ['sum_pos', 'sum_neg', 'min_neg', 'max_pos', 'num_comments', 'has_images', 'has_code_snippets']),
+             ("PosNeg", ['sum_pos', 'sum_neg', 'min_neg', 'max_pos']),
+             ("Sums  ", ['sum_pos', 'sum_neg']),
+             ("MaxMin", ['min_neg', 'max_pos']),
+             ("SumPos", ['sum_pos']),
+             ("SumNeg", ['sum_neg']),
+             ("MinNeg", ['min_neg']),
+             ("MaxPos", ['max_pos']),
+             ("NumCom", ['num_comments']),
+             ("Images", ['has_images']),
+             ("Code  ", ['has_code_snippets']),
+             ("SumCom", ['sum_pos', 'sum_neg', 'num_comments']),
+             ("MM Com", ['min_neg', 'max_pos', 'num_comments'])]
+
+  print_table(files, columns)
+  '''
   print("Issues:")
   print("All    -", to_percent(log_reg('sentiment-issue2.csv', ['sum_pos', 'sum_neg', 'min_neg', 'max_pos', 'num_comments', 'has_images', 'has_code_snippets'])))
   print("PosNeg -", to_percent(log_reg('sentiment-issue2.csv', ['sum_pos', 'sum_neg', 'min_neg', 'max_pos'])))
@@ -57,7 +90,7 @@ if __name__ == '__main__':
   print("Code   -", to_percent(log_reg('sentiment-both.csv', ['has_code_snippets'])))
   print("SumCom -", to_percent(log_reg('sentiment-both.csv', ['sum_pos', 'sum_neg', 'num_comments'])))
   print("MM Com -", to_percent(log_reg('sentiment-both.csv', ['min_neg', 'max_pos', 'num_comments'])))
-  
+  '''
   '''
   print("Issues only:")
   print("All  -", log_reg('sentiment-issue.csv', ['max_pos', 'max_neg', 'has_images', 'has_code_snippets']))
