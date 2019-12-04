725054fb6b7b56b2b2c83a0e05ea25f8a95caa4a
Ryan Nett <rnett@calpoly.edu>
2019-12-04 09:38:02 -0800

    made it an option
    Signed-off-by: Ryan Nett <rnett@calpoly.edu>

--- logistic_regression.py
+++ logistic_regression.py
@@ -3,19 +3,23 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
 
 
-def log_reg(filename):
-  data = pandas.read_csv(filename,
-                         usecols=['max_pos', 'max_neg', 'has_images', 'has_code_snippets'])
-  merged = pandas.read_csv(filename, usecols=['merged'], squeeze=True)
+def log_reg(filename, use_test=True):
+    data = pandas.read_csv(filename,
+                           usecols=['max_pos', 'max_neg', 'has_images', 'has_code_snippets'])
+    merged = pandas.read_csv(filename, usecols=['merged'], squeeze=True)
 
-  X_train, X_test, y_train, y_test = train_test_split(data, merged, test_size=0.20,
-                                                      shuffle=True, random_state=321)
+    X_train, X_test, y_train, y_test = train_test_split(data, merged, test_size=0.20,
+                                                        shuffle=True, random_state=321)
 
-  clf = LogisticRegression().fit(X_train, y_train)
-  print(clf.score(X_test, y_test))
+    clf = LogisticRegression().fit(X_train, y_train)
+
+    if use_test:
+        print(clf.score(X_test, y_test))
+    else:
+        print(clf.score(X_train, y_train))
 
 
 if __name__ == '__main__':
-  log_reg('sentiment-issue.csv')
-  log_reg('sentiment-review.csv')
-  log_reg('sentiment-both.csv')
+    log_reg('sentiment-issue.csv')
+    log_reg('sentiment-review.csv')
+    log_reg('sentiment-both.csv')
