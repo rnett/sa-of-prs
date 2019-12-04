e5620c1203d5838dd1e99e6842951b23fe8445e0
Ryan Nett <rnett@calpoly.edu>
2019-12-04 09:36:32 -0800

    added train/test split
    Signed-off-by: Ryan Nett <rnett@calpoly.edu>

--- logistic_regression.py
+++ logistic_regression.py
@@ -1,13 +1,18 @@
-from sklearn.linear_model import LogisticRegression
 import pandas
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
 
 
 def log_reg(filename):
-  data = pandas.read_csv(filename, 
-    usecols=['max_pos', 'max_neg', 'has_images', 'has_code_snippets'])
+  data = pandas.read_csv(filename,
+                         usecols=['max_pos', 'max_neg', 'has_images', 'has_code_snippets'])
   merged = pandas.read_csv(filename, usecols=['merged'], squeeze=True)
-  clf = LogisticRegression().fit(data, merged)
-  print(clf.score(data,merged))
+
+  X_train, X_test, y_train, y_test = train_test_split(data, merged, test_size=0.20,
+                                                      shuffle=True, random_state=321)
+
+  clf = LogisticRegression().fit(X_train, y_train)
+  print(clf.score(X_test, y_test))
 
 
 if __name__ == '__main__':
